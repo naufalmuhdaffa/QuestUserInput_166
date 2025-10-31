aa86a565e96ae15fea6a7cc8eff4c57be54d85b4
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-10-31 09:11:19 +0700

    menambahkan function

---
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -0,0 +1,9 @@
+package com.example.userinput
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormDataDiri(modifier: Modifier = Modifier) {
+
+}
