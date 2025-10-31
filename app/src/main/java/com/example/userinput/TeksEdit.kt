a6031587e25ef85ed203756a223ecb6d114bae4e
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-10-31 09:20:04 +0700

    menambahkan layout

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -1,11 +1,16 @@
 package com.example.userinput
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormDataDiri(modifier: Modifier) {
@@ -20,4 +25,12 @@ fun FormDataDiri(modifier: Modifier) {
     var jenis by remember { mutableStateOf("") }
 
     val gender: List<String> = listOf("Laki-Laki", "Perempuan")
+
+    Column(modifier = Modifier
+        .padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
 }
