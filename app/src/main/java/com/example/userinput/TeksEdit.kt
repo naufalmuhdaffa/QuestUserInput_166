6def1ca2dd3eae48f3140bb0e55b4971ddb9c74f
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-10-31 09:31:56 +0700

    menambahkan komponen

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -59,6 +60,12 @@ fun FormDataDiri(modifier: Modifier) {
                     ),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
+                    RadioButton(
+                        selected = textJK == item,
+                        onClick = {
+                            textJK = item
+                        }
+                    )
 
                 }
             }
